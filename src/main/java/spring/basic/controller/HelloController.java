6cdf98132bf903e483c621809c5790d6a3240a4f
yuuuunmi <kyuuuunmi@gmail.com>
2017-08-25 11:28:13 +0900

    update JSP Template

--- src/main/java/spring/basic/controller/HelloController.java
+++ src/main/java/spring/basic/controller/HelloController.java
@@ -2,10 +2,12 @@ package spring.basic.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import spring.basic.domain.Hello;
 import spring.basic.util.HelloFactory;
 
@@ -25,9 +27,6 @@ public class HelloController {
     )
     @ResponseBody
     public String hello(@RequestParam("id") int id){
-
-
-
         Hello instance = helloFactory.createInstance();
 
         return instance.toString();
@@ -38,6 +37,32 @@ public class HelloController {
             return "Hello, world! " + id;
         }*/
 
+    }
+
+    @RequestMapping(
+            path = "/",
+            method = {RequestMethod.GET, RequestMethod.POST}
+    )
+    public String index(Model model){
+        // Body가 String 으로 나간다는것 .
+        // ResponseBody가 안붙으면 View가 나감
+
+        model.addAttribute("title", "Hello :D");
+        return "hello";
+    }
+
+    @RequestMapping(
+            path = "/index",
+            method = {RequestMethod.GET, RequestMethod.POST}
+    )
+    public ModelAndView index2(){
+
+        ModelAndView mav = new ModelAndView();
+
+        mav.addObject("title","Hello");
+        ㄴ아.setViewName("hello");
+
+        return mav;
     }
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yuuuunmi
+  Date: 2017. 8. 12.
+  Time: 오전 11:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>${title}</title>
+</head>
+<body>
+
+</body>
+</html>
