7ce15801b00984c1f3cfa4d628126f22c7e7da55
yuuuunmi <kyuuuunmi@gmail.com>
2017-12-27 17:02:56 +0900

    [7.2.1] Application of Service Abstraction - Refactor pom.xml - Abstraction of OXM Service with JAXB and Castor

---
+++ src/main/java/spring/toby1/test/OxmTest.java
@@ -0,0 +1,48 @@
+package spring.toby1.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.XmlMappingException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import spring.toby1.service.jaxb.SqlType;
+import spring.toby1.service.jaxb.Sqlmap;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by yuuuunmi on 2017. 12. 27..
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/OxmTest-context.xml")
+public class OxmTest {
+    @Autowired
+    Unmarshaller unmarshaller;
+
+
+    @Test
+    public void unmarshallSqlMap() throws XmlMappingException, IOException{
+        Source xmlSource = new StreamSource(getClass().getResourceAsStream("/sqlmap.xml"));
+
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+        assertThat(sqlList.get(0).getValue(), is("insert"));
+        assertThat(sqlList.get(1).getKey(), is("get"));
+        assertThat(sqlList.get(1).getValue(), is("select"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+
+    }
+}
